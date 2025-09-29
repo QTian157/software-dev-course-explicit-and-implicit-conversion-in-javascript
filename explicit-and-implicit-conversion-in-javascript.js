3d23abfda34c9060c1f99d57e5e602a6932adbe2
QTian157 <qtian31@gmail.com>
2025-09-29 14:45:37 -0500

    update the file

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -18,15 +18,37 @@ Use console.log() to clearly show the before-and-after type conversions.
 
 */
 
+// --- Task 1: Debugging Type Conversion ---
+// let result = "5" - 2;
+// console.log("The result is: " + result);
 
-let result = "5" - 2;
-console.log("The result is: " + result);
+let resultChange = Number("5") - 2; // apply Number() to implixity to convert string to number
+console.log("The result is: " + resultChange);
 
-let isValid = Boolean("false");
-if (isValid) {
+// let isValid = Boolean("false");
+// if (isValid) {
+//     console.log("This is valid!");
+// }
+
+let isValidChange = Boolean(true); // Boolean("false") is true
+if (isValidChange) {
     console.log("This is valid!");
 }
 
-let age = "25";
-let totalAge = age + 5;
-console.log("Total Age: " + totalAge);
+// let age = "25";
+// let totalAge = age + 5;
+// console.log("Total Age: " + totalAge);
+
+let ageChange = Number("25");
+let totalAgeChange = ageChange + 5;
+console.log("Total Age: " + totalAgeChange);
+
+// --- Task 2: Examples of Type Conversion ---
+// 1. Implicit type conversion
+console.log("2"+2); // 22
+// edge case: empty
+console.log("" + 2); // 2
+// 2. Explicit type conversion
+let age = "22kk";
+console.log(Number(age)); // NaN, becasue "kk" can't conver to number
+console.log(typeof Number(age)); // "number" since NaN is Number type
